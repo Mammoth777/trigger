53484ab41170df33bc06f983dff98a3cb5ee7968
houjiaqi <houjq@engipower.com>
2024-08-23 13:08:34 +0800

    init

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 )
 
-
 func main() {
 	fmt.Println("Server started on port 8080")
 	http.HandleFunc("/", healthCheck)
@@ -26,16 +25,22 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func executeLocalShell(w http.ResponseWriter, r *http.Request) {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		w.Write([]byte("Error getting current directory"))
+		return
+	}
 	queryParams := r.URL.Query()
 	runPath := queryParams.Get("run")
-	file := filepath.Join(".", runPath)
+	file := filepath.Join(dir, runPath)
 	fmt.Println("Executing file: ", file)
 	cmd := exec.Command(file)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
-		w.Write([]byte("Error executing command"))
+		w.Write([]byte("Error executing command" + err.Error()))
 		fmt.Println("Error executing command: ", err)
 	}
 	w.Write([]byte("Command executed"))
